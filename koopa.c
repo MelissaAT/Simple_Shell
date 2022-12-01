e23a91b4009278a57655a10f02969849b5f6df85
GuillermoRosario <guillermorosario628@gmail.com>
2022-12-01 14:07:20 +0000

    koopa

--- koopa.c
+++ koopa.c
@@ -3,15 +3,26 @@
  * main - 
  * Return:
  */
+
 int main()
 {
 	size_t bufsize = 0;
-	char *buffer = NULL;
+	char *buffer = NULL; *token, **tokens = malloc(sizeof(char));
+	int i = 0;
 
 	while(1)
 	{
 		write(1, "$", 2);
 		getline(&buffer, &bufsize, stdin);
+
+		token = strtok(buffer, "\n");
+		while(token)
+		{
+			tokens[i] = token;
+			token = strtok(NULL, "\n");
+			i++
+		}
+		execve(tokens[0], &*tokens, NULL);
 	}
 	return(0);
 }
